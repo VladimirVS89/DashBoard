a438aa16d36246ce3ac89115e517992af504a59e
Villy Villy <VillyVilly71@hotmail.com>
2015-10-08 12:53:17 +0300

    08.10.2015 утро

---
+++ WebDashboard/Dashboard.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Timers;
+
+namespace WebDashboard
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //const double interval60Minutes = 2 * 1000; // milliseconds to one hour
+            //System.Timers.Timer checkForTime = new System.Timers.Timer(interval60Minutes);
+            //checkForTime.Elapsed += new ElapsedEventHandler(checkForTime_Elapsed);
+            //checkForTime.Enabled = true;
+
+            //CountOfCostumers.Text = DateTime.Now.ToString();
+
+        }
+
+        //protected void Button1_Click(object sender, EventArgs e)
+        //{
+        //    CountOfCostumers.Text = DateTime.Now.ToString();
+
+         
+
+           
+        //}
+
+        //void checkForTime_Elapsed(object sender, ElapsedEventArgs e)
+        //{
+    
+        //    CountOfCostumers.Text = DateTime.Now.ToString();
+        
+        //}
+
+
+
+    }
+}
